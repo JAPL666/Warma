a179580e3bc89b3f7d6ba82c87bc72e687a7fe6a
苦艾酒 <37876529+JAPL666@users.noreply.github.com>
2020-10-03 06:34:02 +0800

    自制中文脚本语言

--- src/com/japl/Main.java
+++ src/com/japl/Main.java
@@ -2,8 +2,17 @@ package com.japl;
 
 import com.japl.Warma.Warma;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Warma.run("A:\\Project\\IDEA\\Warma\\执行.warma");
+        Scanner  scanner=new Scanner(System.in);
+        String path=scanner.next();
+        if(new File(path).exists()&&path.contains(".warma")){
+            Warma.run(path);
+        }else{
+            System.out.println("找不到可执行脚本！");
+        }
     }
 }
